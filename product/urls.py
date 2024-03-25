88dd9b23e0930d88dd841590a278c5df0290d0ef
Ahmad-Jaber0 <jaber5834@gmail.com>
2024-03-25 02:54:37 +0200

    ADD Static WetherApi

--- product/urls.py
+++ product/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:pk>/', views.edit_product, name='edit_product'),
     path('delete/<int:pk>/', views.delete_product, name='delete_product'),
-    
+    path('wether/', views.wether, name='wether'),
+
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Product
 from .forms import ProductForm
+import requests
+
+
+def fahrenheit_to_celsius(fahrenheit): 
+    celsius = (fahrenheit - 32) * (5.0/9.0) 
+    
+    return celsius
+
+def wether(request):
+    api_url = 'http://api.openweathermap.org./data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+    city_name="Nablus"
+    url=api_url+city_name
+    response=requests.get(url)
+    content=response.json()
+    city_weather = {
+                'city': city_name,
+                'temperature': fahrenheit_to_celsius(content['main']['temp']),
+                'description': content['weather'][0]['description'],
+                'icon': content['weather'][0]['icon'],
+            }
+    return render(request,"wether.html",city_weather)
+
+
+
 
 
 # /products ---> index .   so we need mapping , go to urls
@@ -16,7 +40,7 @@ def add_product(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index') 
+            #return redirect('index') 
     else:
         form = ProductForm()
     return render(request, 'add_product.html', {'form': form})
@@ -27,7 +51,7 @@ def edit_product(request, pk):
         form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
-            return redirect('index')  
+            #return redirect('index')  
     else:
         form = ProductForm(instance=product)
     return render(request, 'edit_product.html', {'form': form})
@@ -36,5 +60,5 @@ def delete_product(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'POST':
         product.delete()
-        return redirect('index')  
+        #return redirect('index')  
     return render(request, 'delete_product.html', {'product': product})
